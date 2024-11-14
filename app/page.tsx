4f1471cc23c3865c46f07801067903ec5a5a0df5
conorgg123 <conor.ghobrial@gmail.com>
2024-11-14 07:47:00 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
   ];
 
   const allQuotes = [
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "Believe you can and You&apos;re halfway there there. - Theodore Roosevelt",
     "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
     "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
     "The only way to do great work is to love what you do. - Steve Jobs",
